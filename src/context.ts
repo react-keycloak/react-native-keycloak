ad5eb3392afcf573b5fa79b6a81b35aa18e52474
IronTony <zimzum4@gmail.com>
2022-03-03 18:54:03 +0100

    chore: fixed yarn again

--- src/context.ts
+++ src/context.ts
@@ -2,8 +2,9 @@ import { createAuthContext } from '@react-keycloak/core';
 
 import type RNKeycloakInstance from './keycloak/client';
 
-export const reactNativeKeycloakContext =
-  createAuthContext<RNKeycloakInstance>();
+export const reactNativeKeycloakContext = createAuthContext<
+  RNKeycloakInstance
+>();
 
 export const reactNativeKeycloakContextConsumer =
   reactNativeKeycloakContext.Consumer;
